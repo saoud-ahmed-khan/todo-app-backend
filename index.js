b971abd4e1c48a4dd5d7a3d8f3404b5944db20f6
Saoud Ahmed Khan <60979682+saoud-ahmed-khan@users.noreply.github.com>
2021-04-04 13:22:03 +0500

    adding git ignore

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const { dbConnect } = require("./src/database/index");
 const path = require("path");
 const app = express();
 
+
 require("dotenv").config({
   path:
     process.env.NODE_ENV === "development"
@@ -18,10 +19,6 @@ app.get("/", (req, res) => {
 });
 app.use("/api/v1", require("./src/routes/index"));
 const PORT = 5000 || process.env.PORT;
-console.log(process.env.NODE_ENV);
-console.log(process.env.M0NGODB_URI);
-console.log(path.resolve(".env.development"));
-
 
 dbConnect();
 app.listen(PORT, () => {
